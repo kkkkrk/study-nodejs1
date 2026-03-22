6f97345404a706bf8d3a22c4990d4f790351b2ca
김영준 <dudwns1ms1234@gmail.com>
2026-03-22 23:53:01 +0900

    게시글 올리기

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const {MongoClient} = require('mongodb');
 
@@ -29,4 +31,23 @@ app.get('/about', (요청, 응답)=>{
 app.get('/list', async (요청, 응답)=>{
     let result = await db.collection('post').find().toArray()
     응답.render('list.ejs',{글목록: result})
+})
+app.get('/write', (요청, 응답)=>{
+    응답.render('write.ejs')
+})
+app.post('/add', async (요청, 응답) => {
+    try{
+    if(요청.body.title == ''){
+        응답.send("제목 입력 안 함");
+    } else if(요청.body.content == ''){
+        응답.send("내용 입력 안 함");
+    }
+    else{
+        await db.collection('post').insertOne({title: 요청.body.title, content: 요청.body.content})
+        응답.redirect('/list')
+    }
+    } catch(e) {
+        console.log(e)
+        응답.status(500).send('서버에러 남')
+    }
 })
