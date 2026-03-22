f2ebc5048c1df71c6c614fb35c3e6eb7e2fe4889
김영준 <dudwns1ms1234@gmail.com>
2026-03-22 23:11:41 +0900

    nodejs로 mongoDB연결해서 데이터바인딩하기

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+
+const {MongoClient} = require('mongodb');
+
+let db;
+const url = 'mongodb+srv://admin:dudwns123@cluster0.hs2mcpz.mongodb.net/?appName=Cluster0';
+new MongoClient(url).connect().then((client)=>{
+    console.log('DB 연결 성공');
+    db = client.db('forum');
+    app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})}).catch((err)=>{
+    console.log(err);
+})
+app.get('/', (요청, 응답)=>{
+    응답.sendFile(__dirname + '/index.html')
+})
+app.get('/news', (요청, 응답)=>{
+    db.collection('post').insertOne({title:'어쩌구'});
+    응답.send('오늘 비옴')
+})
+app.get('/about', (요청, 응답)=>{
+    응답.sendFile(__dirname + '/introduce.html')
+})
+app.get('/list', async (요청, 응답)=>{
+    let result = await db.collection('post').find().toArray()
+    응답.render('list.ejs',{글목록: result})
+})
